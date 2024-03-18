73223b17e53b9316d6c5a1c30aaed12cb65835d5
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2024-03-18 12:57:30 -0400

    box stack

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,7 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     // creates distance behind vehicle 
     private Vector3 offset = new Vector3(0, 5, -7);
+    
     // Start is called before the first frame update
     void Start()
     {
